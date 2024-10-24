40b7e4023489e57ecb56041ef1c53bfd55b3e78c
ptengaio <167056171+ptengaio@users.noreply.github.com>
2024-10-25 09:55:11 +1000

    Added in all graph codes for thesis

---
+++ Appendix codes.R
@@ -0,0 +1,11 @@
+#Histogram to visualise data distribution and inform approrpiate statistical analyses (refer to thesis Appendix G and H)
+ggplot(coredata, aes( x = mean))+geom_histogram()+facet_grid(season~newgroup)
++ labs(title = "TSSeq (mean) data distribution by site class and season", x = "TSSeq (mg/L)")
+ggplot(coredata, aes( x = TC))+geom_histogram(bins = 40)+facet_grid(season~newgroup) 
++ labs(title = "Total Cover data distribution by site class and season", x = "Total Cover (%)")
+
+Autocorrelation
+#Autocorrelaiton to visually inspect for data individual serial correlation and grouped cross correlation to help inform appropriate statistical analyses (refer to thesis Appendix I, J and K)
+acf(coredata$TC, main = "Autocorrelation of Monthly Total Cover", lwd=2)
+acf(coredata$mean, main = "Autocorrelation of Monthly Mean TSSeq Values", lwd=2)
+ccf(coredata$TC,coredata$mean, main = "Cross-correlation between monthly mean TSSeq and Total Cover", lwd=2)
---
+++ TSS against Total Cover.R
@@ -0,0 +1,21 @@
+#Graph 1 code for scatter plot of TSSeq against TC with regression lines for each site class and season 
+ggplot(data = coredata, aes(x = n2TC, y = n2mean, color = newgroup, shape = season)) + 
+  geom_point(alpha = 0.6) + 
+  geom_smooth(method = "lm", se = FALSE, aes(linetype = season, color = newgroup)) + 
+  theme_minimal() + 
+  labs(
+    title = "Relationship between Riparian Total Cover and Total Suspended Sediment across site class and season", subtitle = "December 2020 - December 2023", 
+    x = "natural log of % total cover", 
+    y = "natural log of TSSeq (mg/L)", 
+    color = "Site Class", 
+    linetype = "Regression line"
+  ) + 
+  theme(
+    legend.position = "right", 
+    legend.title = element_text(size = 14), 
+    legend.text = element_text(size = 12), 
+    plot.title = element_text(size = 16, face = "bold"), 
+    plot.subtitle = element_text(size = 14)
+  ) + 
+  scale_color_manual(values = c("red3", "chartreuse4", "dodgerblue2")) + 
+  scale_linetype_manual(values = c("solid", "dashed"))
